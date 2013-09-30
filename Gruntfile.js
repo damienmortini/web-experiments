36a2a286982f47ede677cfabf45d3ec42c0ff162
Damien Mortini <ddotmnn+git@gmail.com>
2013-09-30 18:51:57 +0100

    update to grunt

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        watch: {
+            livereload: {
+                options: {
+                    livereload: '<%= connect.livereload.options.livereload %>'
+                },
+                files: [
+                    '**/*'
+                ]
+            }
+        },
+        connect: {
+            livereload: {
+                options: {
+                    port: 9000,
+                    livereload: 35729,
+                    hostname: '*',
+                    // open: 'http://localhost:9000',
+                    base: [
+                        'libs',
+                        'experiments'
+                    ]
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('server', [
+        'connect',
+        'watch'
+    ]);
+};
