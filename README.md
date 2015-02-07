9c34770e93b670ada8c74750481c8eb8f574064f
Damien Mortini <ddotmnn+git@gmail.com>
2015-02-07 19:55:40 +0000

    update build system with gulp

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Web Experiments
 
 Some experiments on the internet.
 
-`npm run install` > `npm run serve`
+`npm install && bower install` > `gulp serve`
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var to5 = require('gulp-6to5');
+
+gulp.task('scripts', function () {
+    return gulp.src('experiments/**/*.js')
+        .pipe(to5({
+          modules: 'amd'
+        }))
+        .pipe(gulp.dest('.tmp'));
+});
+
+gulp.task('serve', ['scripts'], function () {
+  browserSync({
+    notify: false,
+    port: 9000,
+    directory: true,
+    server: {
+      baseDir: ['.tmp', 'experiments'],
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
+  });
+
+  gulp.watch([
+    'experiments/**/*',
+    '.tmp/**/*'
+  ]).on('change', browserSync.reload);
+
+  gulp.watch('experiments/**/*.js', ['scripts']);
+});
