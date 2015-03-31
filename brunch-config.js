43a0f370ba0591ec63160083f9c8303ffe3e3260
Damien Mortini <ddotmnn+git@gmail.com>
2015-03-31 04:02:32 +0100

    use babelify for xps / add polyfill

--- brunch-config.js
+++ brunch-config.js
@@ -1,14 +1,24 @@
 exports.config = {
-  conventions: {
-    assets: [
-      /app[\\/](?!scripts|styles)/
+  paths: {
+    watched: [
+      'app',
+      'node_modules/babel-brunch/node_modules/babel-core/browser-polyfill.js'
     ]
+  },
+  conventions: {
+    assets: function (path) {
+      return /^app/.test(path) && !/scripts|styles/.test(path);
+    }
   },
   files: {
     javascripts: {
       joinTo: {
-        'scripts/vendor.js': /^bower_components/,
-        'scripts/main.js': /^app/
+        'scripts/vendor.js': [
+          'node_modules/babel-brunch/node_modules/babel-core/browser-polyfill.js',
+          /^bower_components/
+        ],
+        'scripts/main.js': /^app[\\/]scripts/,
+        'experiments/raymarch-physic/scripts/main.js': /^app[\\/]experiments[\\/]raymarch-physic[\\/]scripts/
       },
       order: {
         before: ['bower_components/threejs/build/three.js']
@@ -20,6 +30,8 @@ exports.config = {
   },
   onCompile: function() {
     'use strict';
+    require('fs').appendFile('public/scripts/vendor.js', '\n\nrequire(\'node_modules/babel-brunch/node_modules/babel-core/browser-polyfill\');');
     require('fs').appendFile('public/scripts/main.js', '\n\nrequire(\'scripts/main\');');
+    require('fs').appendFile('public/experiments/raymarch-physic/scripts/main.js', '\n\nrequire(\'experiments/raymarch-physic/scripts/main\');');
   }
 };
